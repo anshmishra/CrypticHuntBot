271f4c38118299a68da3c97869660ba780b19fee
anshmishra <anshmishra703@gmail.com>
2020-08-15 00:03:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -136,19 +136,19 @@ client.on('message', (message) => {
 })
 
 client.on('message', (message) => {
-        if (message.content == 'mcs! start'){
+        if (message.content == 'mcs! start')
                 message.react('✅')
                 message.reply('Unlocking <#743524266942791680> text chat for you soon, your answers (if correct) will unlock the text channel for the next level automatically. In case of any issues related to the bot, please contact the bot owner/moderator <@444522639499853824> and in case of any other issues related to the event, contact the event head <@600649783673225216>. GOOD LUCK!')
                 message.member.roles.add(Test_1)
         
-        }
+        
 })
 
 client.on('message', (message) => {
-        if (message.content == 'test_msg_00'){
+        if (message.content == 'test_msg_00')
                 message.reply('deployed on application platform and pushed master on github')
                 message.react('✅')
-        }
+        
 })
 
 client.login(process.env.token)
